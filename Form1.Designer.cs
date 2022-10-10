6748f61692f487edb0e60171973fb31526ad05f0
旭 张 <360901711@qq.com>
2022-10-10 21:41:37 +0800

    1.添加一些按钮，清空，停止等 2.添加一个右键菜单用于删除列表 3.音乐文件改为循环播放

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,35 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.btnOpen = new System.Windows.Forms.Button();
             this.listMusicView = new System.Windows.Forms.ListBox();
+            this.cMS_listMV = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.delSingleSong = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearAllSongs = new System.Windows.Forms.ToolStripMenuItem();
             this.btnPrev = new System.Windows.Forms.Button();
             this.btnNext = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cMS_listMV.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnOpen
             // 
-            this.btnOpen.Location = new System.Drawing.Point(7, 6);
+            this.btnOpen.Location = new System.Drawing.Point(8, 9);
             this.btnOpen.Name = "btnOpen";
             this.btnOpen.Size = new System.Drawing.Size(94, 29);
             this.btnOpen.TabIndex = 0;
-            this.btnOpen.Text = "Open";
+            this.btnOpen.Text = "打开文件";
             this.btnOpen.UseVisualStyleBackColor = true;
             this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
             // 
             // listMusicView
             // 
+            this.listMusicView.ContextMenuStrip = this.cMS_listMV;
             this.listMusicView.FormattingEnabled = true;
+            this.listMusicView.HorizontalScrollbar = true;
             this.listMusicView.ItemHeight = 20;
-            this.listMusicView.Location = new System.Drawing.Point(7, 41);
+            this.listMusicView.Location = new System.Drawing.Point(8, 44);
             this.listMusicView.Name = "listMusicView";
             this.listMusicView.Size = new System.Drawing.Size(294, 304);
             this.listMusicView.TabIndex = 1;
             this.listMusicView.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listMusicView_MouseDoubleClick);
             // 
+            // cMS_listMV
+            // 
+            this.cMS_listMV.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.cMS_listMV.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.delSingleSong,
+            this.clearAllSongs});
+            this.cMS_listMV.Name = "contextMenuStrip1";
+            this.cMS_listMV.Size = new System.Drawing.Size(139, 52);
+            this.cMS_listMV.Opening += new System.ComponentModel.CancelEventHandler(this.cMS_listMV_Opening);
+            // 
+            // delSingleSong
+            // 
+            this.delSingleSong.Name = "delSingleSong";
+            this.delSingleSong.Size = new System.Drawing.Size(138, 24);
+            this.delSingleSong.Text = "删除";
+            this.delSingleSong.Click += new System.EventHandler(this.delSingleSong_Click);
+            // 
+            // clearAllSongs
+            // 
+            this.clearAllSongs.Name = "clearAllSongs";
+            this.clearAllSongs.Size = new System.Drawing.Size(138, 24);
+            this.clearAllSongs.Text = "清空列表";
+            this.clearAllSongs.Click += new System.EventHandler(this.delAllSongs_Click);
+            // 
             // btnPrev
             // 
-            this.btnPrev.Location = new System.Drawing.Point(107, 6);
+            this.btnPrev.Location = new System.Drawing.Point(308, 44);
             this.btnPrev.Name = "btnPrev";
             this.btnPrev.Size = new System.Drawing.Size(94, 29);
             this.btnPrev.TabIndex = 2;
@@ -66,7 +101,7 @@
             // 
             // btnNext
             // 
-            this.btnNext.Location = new System.Drawing.Point(207, 6);
+            this.btnNext.Location = new System.Drawing.Point(308, 79);
             this.btnNext.Name = "btnNext";
             this.btnNext.Size = new System.Drawing.Size(94, 29);
             this.btnNext.TabIndex = 3;
@@ -74,20 +109,67 @@
             this.btnNext.UseVisualStyleBackColor = true;
             this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
             // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(308, 9);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(94, 29);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "关闭应用";
+            this.btnClose.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(308, 114);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(94, 29);
+            this.btnStop.TabIndex = 5;
+            this.btnStop.Text = "停止";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(308, 149);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(94, 29);
+            this.btnClear.TabIndex = 6;
+            this.btnClear.Text = "清空列表";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(313, 328);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "仅支持wav";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(312, 352);
+            this.ClientSize = new System.Drawing.Size(406, 356);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnClose);
             this.Controls.Add(this.btnNext);
             this.Controls.Add(this.btnPrev);
             this.Controls.Add(this.listMusicView);
             this.Controls.Add(this.btnOpen);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "MusicPlayer";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.cMS_listMV.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -97,5 +179,12 @@
         private ListBox listMusicView;
         private Button btnPrev;
         private Button btnNext;
+        private Button btnClose;
+        private Button btnStop;
+        private ContextMenuStrip cMS_listMV;
+        private Button btnClear;
+        private ToolStripMenuItem delSingleSong;
+        private ToolStripMenuItem clearAllSongs;
+        private Label label2;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -10,20 +10,20 @@ namespace MusicPlayer
         }
 
         List<string> listSongs = new List<string>();
-        SoundPlayer sp = new SoundPlayer();
+        SoundPlayer sp = new();
 
         private void btnOpen_Click(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
             ofd.Title = "打开文件";
-            ofd.InitialDirectory = @"C:\Users\VEY\Desktop\Pictures";
+            ofd.InitialDirectory = @"C:\Users\VEY\Desktop\Picture";
             ofd.Multiselect = true;
             ofd.Filter = "音乐|*.wav|所有文件|*.*";
             ofd.ShowDialog();
             string[] path = ofd.FileNames;
             for(int i = 0; i < path.Length; i++)
             {
-                listMusicView.Items.Add(Path.GetFileName(path[i]));
+                listMusicView.Items.Add(Path.GetFileNameWithoutExtension(path[i]));
                 listSongs.Add(path[i]);
             }
         }
@@ -31,7 +31,7 @@ namespace MusicPlayer
         private void listMusicView_MouseDoubleClick(object sender, MouseEventArgs e)
         {
             sp.SoundLocation = listSongs[listMusicView.SelectedIndex];
-            sp.Play();
+            sp.PlayLooping();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -51,7 +51,7 @@ namespace MusicPlayer
             listMusicView.SelectedIndex = index;
 
             sp.SoundLocation = listSongs[index];
-            sp.Play();
+            sp.PlayLooping();
         }
 
         private void btnPrev_Click(object sender, EventArgs e)
@@ -64,10 +64,55 @@ namespace MusicPlayer
             }
 
             listMusicView.SelectedIndex = index;
-            
-
             sp.SoundLocation = listSongs[index];
-            sp.Play();
+            sp.PlayLooping();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            sp.Stop();
+        }
+
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            listSongs.Clear();
+            listMusicView.Items.Clear();
+        }
+        
+
+
+
+        private void cMS_listMV_Opening(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+            if (listMusicView.SelectedIndex == -1)
+            {
+                delSingleSong.Visible = false;
+            }
+            else
+            {
+                delSingleSong.Visible = true;
+            }
+            
+        }
+
+        private void delAllSongs_Click(object sender, EventArgs e)
+        {
+            listSongs.Clear();
+            listMusicView.Items.Clear();
+        }
+
+        private void delSingleSong_Click(object sender, EventArgs e)
+        {
+            int index = listMusicView.SelectedIndex;
+            listSongs.Remove(listSongs[index]);
+            listMusicView.Items.RemoveAt(index);
         }
     }
 }
